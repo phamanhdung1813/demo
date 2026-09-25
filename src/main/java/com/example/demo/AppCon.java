566352c5900b7c279570f4383400dc7e00313571
phamanhdung1813 <phamanhdung1813@gmail.com>
2026-09-25 14:30:55 +0700

    Change something

--- src/main/java/com/example/demo/AppCon.java
+++ src/main/java/com/example/demo/AppCon.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +16,12 @@ public class AppCon {
 
     @GetMapping("/test")
     public String test() {
-        appService.serviceAPlus();
+        appService.methodA();
         return "OK";
+    }
+
+    @GetMapping("/all")
+    public Iterable<AppEntity> getAll() {
+        return appService.getAll();
     }
 }
--- src/main/java/com/example/demo/AppService.java
+++ src/main/java/com/example/demo/AppService.java
@@ -1,6 +1,13 @@
 package com.example.demo;
 
+import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,16 +17,48 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class AppService {
+    private static final Logger log =
+            LoggerFactory.getLogger(AppService.class);
+    @Autowired
+    private io.micrometer.tracing.Tracer tracer;
+
+    @Lazy
+    @Autowired
+    private AppService self;
 
     private final AppRepository repository;
 
     @Transactional(propagation = Propagation.REQUIRED)
-    public void serviceAPlus() {
+    public void methodA() {
+        var span = tracer.currentSpan();
+        log.info("methodA currentSpanId={}", span);
+        log.info("methodA traceId={}", MDC.get("traceId"));
+        log.info("Thread Transaction on  methodA={}", Thread.currentThread().threadId());
         Optional<AppEntity> currentEntity = repository.findById(1L);
         if (currentEntity.isPresent()) {
             AppEntity a = currentEntity.get();
             a.setAmount(a.getAmount() + 100L);
             repository.save(a);
+            self.methodB();
         }
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void methodB() {
+        var span = tracer.currentSpan();
+        log.info("methodB currentSpanId={}", span);
+        log.info("methodB traceId={}", MDC.get("traceId"));
+        log.info("Thread Transaction on  methodB={}", Thread.currentThread().threadId());
+        Optional<AppEntity> currentEntity = repository.findById(1L);
+        if (currentEntity.isPresent()) {
+            AppEntity a = currentEntity.get();
+            a.setAmount(a.getAmount() - 200L);
+            repository.save(a);
+            throw new RuntimeException("Intentional Exception in methodB");
+        }
+    }
+
+    public Iterable<AppEntity> getAll() {
+        return repository.findAll();
     }
 }
